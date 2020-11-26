2d1791925768369c88565b8161537f3d45fb81c3
newbeeguy <mskjavascript@gmail.com>
2020-11-26 18:06:35 +0300

    complete test hw

--- src/01-hello-tests/01.ts
+++ src/01-hello-tests/01.ts
@@ -1,1 +1,26 @@
 
+export function sum(a:number, b: number) {
+    return a + b;
+}
+
+
+export function mult(a:number, b: number) {
+    return a * b;
+}
+
+
+
+
+
+
+
+
+export function splitIntoWords(sentense: string) {
+    return  sentense.toLowerCase().split(" ")
+        .filter( word => word !== "")
+        .map( w => w.replace("!", "")
+            .replace(".", "")
+            .replace(",", ""))
+}
+
+
